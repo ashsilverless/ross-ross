cf909d0df10de27f81d073296e3cbf8e26d58a6a
Ash - SIlverless <ash@silverless.co.uk>
2020-06-05 15:17:37 +0100

    DEV - Blog Title

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <meta name="keywords" content=" ">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>Ross & Ross</title>
+<title><?php bloginfo( 'name' ); ?></title>
 
 <link rel="stylesheet" href="https://use.typekit.net/axv0hre.css"><!--TYPEKIT INJECT-->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
